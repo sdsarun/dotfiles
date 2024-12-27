f1bf0bfa8739bb43fea559bf85804fe7d4a867d0
Sarun Daunghirun <125266560+sdsarun@users.noreply.github.com>
2024-12-27 23:20:33 +0700

    fix(wezterm): change color schema

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config = {
-	color_scheme = "Monokai Soda",
+	color_scheme = "Argonaut (Gogh)",
 	font = wezterm.font("Hack Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }),
 	font_size = 14,
 	window_background_opacity = 1,
