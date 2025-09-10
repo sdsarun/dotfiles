b2dfa7a46eadbd6941d91f27b1176682d16a7cca
Sarun Daunghirun <sdsarun@outlook.com>
2025-09-10 19:27:20 +0700

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 DOTVERSE=~/.dotverse
 
+[ -f ~/.zshrc ] && rm ~/.zshrc
+[ -f ~/.tmux.conf ] && rm ~/.tmux.conf
+[ -f ~/.wezterm.lua ] && rm ~/.wezterm.lua
+
 ln -sf $DOTVERSE/zsh/.zshrc ~/.zshrc
 ln -sf $DOTVERSE/tmux/.tmux.conf ~/.tmux.conf
 ln -sf $DOTVERSE/wezterm/.wezterm.lua ~/.wezterm.lua
+
+echo "Symlinks created successfully!"
