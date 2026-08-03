65a326751d0fa1bce9c5779a9a7a3633b88fd890
Sarun Daunghirun <sdsarun@outlook.com>
2026-08-03 18:47:32 +0700

    feat: update configs

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,12 +2,14 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config = {
-        color_scheme = "Abernathy",
-        font = wezterm.font("Hack Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }),
-        font_size = 14,
-        window_background_opacity = 1,
-        animation_fps = 1,
-        max_fps = 120,
+	color_scheme = "Abernathy",
+	font = wezterm.font("CommitMono Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }),
+	font_size = 14,
+	window_background_opacity = 1,
+	animation_fps = 1,
+	max_fps = 240,
+	hide_tab_bar_if_only_one_tab = true,
+	tab_bar_at_bottom = true,
 }
 
 return config
